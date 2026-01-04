0794499dfa3e4abdcb83e99a92dc81b1dc172014
Nikita <fomin.nv@phystech.edu>
2026-01-04 05:29:55 +0300

    settings update

--- geo/settings.py
+++ geo/settings.py
@@ -74,8 +74,11 @@ WSGI_APPLICATION = "geo.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blog_db",
+        'USER': 'blog',
+        'PASSWORD': 'blog',
+        'HOST': 'localhost',
     }
 }
 
