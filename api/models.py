7f3923f7c39838dbef6fceae7bc33a95223fbe95
Nikita <fomin.nv@phystech.edu>
2026-01-04 06:09:12 +0300

    models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Point(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+
+class Message(models.Model):
+    message = models.TextField(max_length=500)
+    point = models.ForeignKey(Point, on_delete=models.CASCADE)
