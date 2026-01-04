5db3937ce1441b5c61dfa806134bfa6d8d4dd030
Nikita <fomin.nv@phystech.edu>
2026-01-04 06:22:48 +0300

    PointSerializer

---
+++ api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import Point
+
+
+class PointSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Point
+        fields = [
+            'name',
+            'longitude',
+            'latitude',
+        ]
